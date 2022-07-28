7bb5bcf14f15122b1320f4af5ab1ed5073a8d0dd
yasuaki-344 <miyoshi.yh@om.asahi-kasei.co.jp>
2022-07-28 18:45:57 +0900

    Create unit test

---
+++ test/ApplicationCore.Test/WeatherForecastServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationCore.Dto;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Services;
+using AutoMapper;
+
+namespace ApplicationCore.Test;
+
+public class WeatherForecastServiceTest
+{
+    private readonly Mock<IMapper> _mapper;
+    private readonly Mock<IWeatherForecastRepository> _repository;
+    private readonly WeatherForecastService _target;
+
+    public WeatherForecastServiceTest()
+    {
+        _mapper = new Mock<IMapper>();
+        _repository = new Mock<IWeatherForecastRepository>();
+        _target = new WeatherForecastService(_mapper.Object, _repository.Object);
+    }
+
+    [Fact]
+    public void Map_GivenWeatherForecastEntity_ReturnsDto()
+    {
+        var actual = _target.GetWeatherForecasts();
+
+        _repository.Verify(mock => mock.GetWeatherForecasts(), Times.Once());
+        _mapper.Verify(mock => mock.Map<IEnumerable<WeatherForecast>, WeatherForecastDto[]>(It.IsAny<IEnumerable<WeatherForecast>>()), Times.Once());
+    }
+}
---
+++ test/Infrastructure.Test/WeatherForecastRepositoryTest.cs
@@ -0,0 +1,49 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Test;
+
+public class WeatherForecastRepositoryTest : IDisposable
+{
+    private readonly DatabaseContext _context;
+    private readonly WeatherForecastRepository _target;
+
+    public WeatherForecastRepositoryTest()
+    {
+        var options = new DbContextOptionsBuilder<DatabaseContext>()
+            .UseInMemoryDatabase(databaseName: "WeatherForecastRepositoryTestDB")
+            .Options;
+        _context = new DatabaseContext(options);
+        _target = new WeatherForecastRepository(_context);
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+    }
+
+    [Fact]
+    public async Task ContactExists_NonExistentId_ReturnsEntities()
+    {
+        await _context.WeatherForecasts.AddRangeAsync(new[]
+        {
+            new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = 10,
+                Summary = string.Empty
+            },
+            new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(1),
+                TemperatureC = 20,
+                Summary = "Chilly"
+            }
+        });
+        await _context.SaveChangesAsync();
+
+        var actual = _target.GetWeatherForecasts();
+
+        Assert.NotEmpty(actual);
+    }
+}
