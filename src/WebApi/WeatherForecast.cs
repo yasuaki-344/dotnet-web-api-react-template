48248fb127eb46a3f8c47d48a109b568634ae676
yasuaki-344 <miyoshi.yh@om.asahi-kasei.co.jp>
2022-07-28 16:00:59 +0900

    Add json attoribute setting

--- src/WebApi/WeatherForecast.cs
+++ src/WebApi/WeatherForecast.cs
@@ -1,16 +1,20 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
 
 namespace WebApi;
 
+[DisplayName("WeatherForecast")]
 public class WeatherForecast
 {
+    [JsonPropertyName("date")]
     public DateTime Date { get; set; }
 
+    [JsonPropertyName("temperature_c")]
     public int TemperatureC { get; set; }
 
+    [JsonPropertyName("temperature_f")]
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+    [JsonPropertyName("summary")]
+    public string Summary { get; set; } = string.Empty;
 }
