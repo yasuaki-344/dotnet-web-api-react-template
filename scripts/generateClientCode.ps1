fd5060bad99002d56fb13fdf96f2dc725a6e263d
yasuaki-344 <miyoshi.yh@om.asahi-kasei.co.jp>
2022-07-29 10:16:10 +0900

    Modify openapitools.json

--- scripts/generateClientCode.ps1
+++
@@ -1,6 +0,0 @@
-Invoke-WebRequest http://localhost:5218/swagger/v1/swagger.yaml -OutFile docs//swagger.yml
-npx @openapitools/openapi-generator-cli generate `
-  -i docs//swagger.yml `
-  -g typescript-fetch `
-  -o src//ClientApp//src//api-gateways `
-  --additional-properties=modelPropertyNaming=camelCase,supportsES6=true,typescriptThreePlus=true,withInterfaces=true
