d4de1e1a7abcab4410cd56ea920eb39837f6d68c
yasuaki-344 <ikausay.443@gmail.com>
2022-12-04 17:01:15 +0900

    Implement useContuer

--- src/ClientApp/src/services/CountAdapter.ts
+++ src/ClientApp/src/services/CountAdapter.ts
@@ -1,5 +1,10 @@
 import { CountService } from "../application";
+import { useCountStorage } from "./StorageAdapter";
 
-export const useCounter = (): CountService => ({
-    increaseCount: () => console.log("this is test"),
-  });
+export const useCounter = (): CountService => {
+  const { count, updateCount } = useCountStorage();
+
+  return {
+    increaseCount: () => updateCount(count + 1),
+  };
+};
