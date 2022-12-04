c760ca1e989eb7a1ecd884c7196f6d26a33746b7
yasuaki-344 <ikausay.443@gmail.com>
2022-12-04 16:27:51 +0900

    Create Storage context

---
+++ src/ClientApp/src/services/Storage.tsx
@@ -0,0 +1,20 @@
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
+
+const StorageContext = createContext<any>({});
+export const useStore = () => useContext(StorageContext);
+
+export const Provider = ({ children }: { children: ReactNode }) => {
+  const [count, setCount] = useState(10);
+
+  const value = useMemo(() => ({ count, updateCount: setCount }), [count]);
+
+  return (
+    <StorageContext.Provider value={value}>{children}</StorageContext.Provider>
+  );
+};
