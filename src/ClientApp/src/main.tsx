1e77e62e0c47baeb521e73bd3ecc944d0c2bc836
yasuaki-344 <ikausay.443@gmail.com>
2022-12-04 16:29:21 +0900

    Add storage provider to main.tsx

--- src/ClientApp/src/main.tsx
+++ src/ClientApp/src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { Provider } from "./services";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
